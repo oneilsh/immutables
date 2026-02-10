6d430cbad3189ee2473b2d74faba0763d6dd45f2
Shawn T O'Neil <oneilsh@gmail.com>
2026-02-09 19:19:03 -0500

    more tests

---
+++ tests/testthat/test-invariants.R
@@ -0,0 +1,87 @@
+validate_fingertree_invariants <- function(t) {
+  is_homogeneous <- function(xs) {
+    if(length(xs) <= 1) {
+      return(TRUE)
+    }
+    kinds <- vapply(xs, is_structural_node, logical(1))
+    all(kinds == kinds[[1]])
+  }
+
+  walk <- function(x) {
+    if(x %isa% Empty) {
+      return(invisible(TRUE))
+    }
+
+    if(x %isa% Single) {
+      testthat::expect_identical(length(x), 1L)
+      return(invisible(TRUE))
+    }
+
+    if(x %isa% Deep) {
+      testthat::expect_true(length(x$prefix) >= 1 && length(x$prefix) <= 4)
+      testthat::expect_true(length(x$suffix) >= 1 && length(x$suffix) <= 4)
+      testthat::expect_true(is_homogeneous(x$prefix))
+      testthat::expect_true(is_homogeneous(x$suffix))
+      walk(x$middle)
+      return(invisible(TRUE))
+    }
+
+    if(x %isa% Node) {
+      testthat::expect_true(length(x) %in% c(2L, 3L))
+      testthat::expect_true(is_homogeneous(x))
+      for(el in x) {
+        if(is_structural_node(el)) {
+          walk(el)
+        }
+      }
+      return(invisible(TRUE))
+    }
+
+    if(x %isa% Digit) {
+      testthat::expect_true(length(x) >= 1 && length(x) <= 4)
+      testthat::expect_true(is_homogeneous(x))
+      for(el in x) {
+        if(is_structural_node(el)) {
+          walk(el)
+        }
+      }
+      return(invisible(TRUE))
+    }
+  }
+
+  walk(t)
+  invisible(TRUE)
+}
+
+testthat::test_that("structural invariants hold after mixed updates", {
+  t <- empty_tree()
+  for(i in 1:30) {
+    if(i %% 2 == 0) {
+      t <- append(t, letters[i %% 26 + 1])
+    } else {
+      t <- prepend(t, letters[i %% 26 + 1])
+    }
+  }
+  validate_fingertree_invariants(t)
+})
+
+testthat::test_that("structural invariants hold after concat and split", {
+  mr <- MeasureReducer(function(a, b) a + b, 0, function(el) 1)
+  t1 <- tree_from(letters[1:12], reducer = mr)
+  t2 <- tree_from(letters[13:20], reducer = mr)
+  t <- concat_trees(t1, t2)
+  validate_fingertree_invariants(t)
+
+  s <- split(t, function(v) v >= 10)
+  validate_fingertree_invariants(s$left)
+  validate_fingertree_invariants(s$right)
+})
+
+testthat::test_that("measured deep trees retain root measure through updates", {
+  mr <- MeasureReducer(function(a, b) a + b, 0, function(el) 1)
+  t <- tree_from(letters[1:10], reducer = mr)
+  t <- prepend(t, "z")
+  t <- append(t, "y")
+  testthat::expect_identical(attr(t, "measure"), 12)
+  validate_fingertree_invariants(t)
+})
---
+++ tests/testthat/test-view.R
@@ -0,0 +1,50 @@
+tree_chars <- function(t) {
+  reduce_left(t, Reducer(function(a, b) paste0(a, b), ""))
+}
+
+testthat::test_that("viewL and viewR return boundary element and remainder", {
+  t <- tree_from(letters[1:5])
+
+  vl <- fingertree:::viewL(t)
+  testthat::expect_identical(vl$elem, "a")
+  testthat::expect_identical(tree_chars(vl$rest), "bcde")
+
+  vr <- fingertree:::viewR(t)
+  testthat::expect_identical(vr$elem, "e")
+  testthat::expect_identical(tree_chars(vr$rest), "abcd")
+})
+
+testthat::test_that("viewL/viewR on Single produce Empty remainder", {
+  t <- tree_from("x")
+
+  vl <- fingertree:::viewL(t)
+  vr <- fingertree:::viewR(t)
+
+  testthat::expect_identical(vl$elem, "x")
+  testthat::expect_identical(vr$elem, "x")
+  testthat::expect_true(vl$rest %isa% Empty)
+  testthat::expect_true(vr$rest %isa% Empty)
+})
+
+testthat::test_that("deepL/deepR collapse correctly when middle is Empty", {
+  pr <- list()
+  sf <- Digit("c", "d")
+  t1 <- fingertree:::deepL(pr, Empty(), sf)
+  testthat::expect_identical(tree_chars(t1), "cd")
+
+  pr2 <- Digit("a", "b")
+  sf2 <- list()
+  t2 <- fingertree:::deepR(pr2, Empty(), sf2)
+  testthat::expect_identical(tree_chars(t2), "ab")
+})
+
+testthat::test_that("measured view helpers preserve measure attributes", {
+  mr <- MeasureReducer(function(a, b) a + b, 0, function(el) 1)
+  t <- tree_from(letters[1:4], reducer = mr)
+
+  vl <- fingertree:::viewL(t, mr)
+  vr <- fingertree:::viewR(t, mr)
+
+  testthat::expect_identical(attr(vl$rest, "measure"), 3)
+  testthat::expect_identical(attr(vr$rest, "measure"), 3)
+})
